608516de9fec2cf8326c955b5bf57c0c57c6debb
matej-pericic <matej.pericic@gmail.com>
2024-03-03 21:30:19 +0100

    Editor settings + minor script changes, final commit for the day

--- Scripts/MinigameTemplate.gd
+++ Scripts/MinigameTemplate.gd
@@ -17,14 +17,14 @@ func _input(_event: InputEvent) -> void:
 		var mousePos: Vector2 = get_viewport().get_mouse_position()
 		var mouseCollision: Vector3 = ScreenPointToRay(mousePos)[0]
 		var mouseCollider: RigidBody3D = ScreenPointToRay(mousePos)[1]
-		
+
 		if mouseCollider != null:
 			grabPivot.position = mouseCollision
 			grabJoint.position = grabPivot.position
 			grabJoint.node_a = mouseCollider.get_path()
 			grabJoint.node_b = grabPivot.get_path()
 			grabState = true
-	
+
 	if Input.is_action_just_released("left_click"):
 		grabState = false
 		grabJoint.node_a = ""
@@ -35,10 +35,10 @@ func ScreenPointToRay(mousePos: Vector2) -> Array: # Grabs a RigidBody3D
 	var rayOrigin: Vector3 = camera.project_ray_origin(mousePos)
 	var rayEnd: Vector3 = rayOrigin + camera.project_ray_normal(mousePos) * RAY_LENGTH
 	var rayDict: Dictionary = spaceState.intersect_ray(PhysicsRayQueryParameters3D.create(rayOrigin, rayEnd))
-	
+
 	if rayDict.has("position") && rayDict["collider"] in interactiveObjects:
 		return [rayDict["position"], rayDict["collider"]]
-	
+
 	return [Vector3(), null, Vector3()]
 
 func _physics_process(delta: float) -> void:
@@ -46,7 +46,7 @@ func _physics_process(delta: float) -> void:
 	if grabState:
 		var grabMousePos: Vector2 = get_viewport().get_mouse_position()
 		var grabRayOrigin: Vector3 = camera.project_ray_origin(grabMousePos)
-		var grabRayLength: float = grabRayOrigin.distance_to(grabPivot.position)
+		var grabRayLength: float = grabRayOrigin.distance_to(grabJoint.position)
 		var grabRayEnd: Vector3 = grabRayOrigin + camera.project_ray_normal(grabMousePos) * grabRayLength
-		
+
 		grabPivot.position = lerp(grabPivot.position, grabRayEnd, lerpStrength * delta)
